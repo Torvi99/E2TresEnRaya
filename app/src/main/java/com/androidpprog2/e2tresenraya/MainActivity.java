c690a88c7abb057d945b4ed2f95568aa3ea841bf
Torvi99 <torvi1969@gmail.com>
2022-02-27 21:33:14 +0100

    Error controller

--- app/src/main/java/com/androidpprog2/e2tresenraya/MainActivity.java
+++ app/src/main/java/com/androidpprog2/e2tresenraya/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
     int turno = 0;
+    int[] position = new int[]{
+            R.id.button,R.id.button2,R.id.button3,
+            R.id.button4,R.id.button5,R.id.button6,
+            R.id.button7,R.id.button8,R.id.button9
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +27,33 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClickButton(View v){
+        System.out.println(v.getId());
+        int controller = 0;
 
-        if(turno == 0){
-            v.setBackgroundResource(R.drawable.o);
-            turno = 1;
-            text.setText("Player 2:");
-        }else if(turno == 1){
-            v.setBackgroundResource(R.drawable.x);
-            turno = 0;
-            text.setText("Player 1:");
+
+        for(int x = 0; x < position.length; x++){
+            if(position[x] == v.getId()){
+                controller = 1;
+                position[x] = 0;
+            }
+
         }
+
+        if(controller == 1){
+            if(turno == 0){
+                v.setBackgroundResource(R.drawable.o);
+                turno = 1;
+                text.setText("Player 2:");
+            }else if(turno == 1){
+                v.setBackgroundResource(R.drawable.x);
+                turno = 0;
+                text.setText("Player 1:");
+            }
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT);
+            toast.show();
+            System.out.println("Error");
+        }
+
     }
 }
